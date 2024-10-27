4b319680c54d3f192f5e77c90ecee920a038d328
developerpavani <juvvalapatipavani02@gmail.com>
2024-10-27 12:49:33 +0530

    developed 2 functions ie add and multi

---
+++ Cal.java
@@ -0,0 +1,12 @@
+class cal
+{
+    static double add(double n1, double n1)
+    {
+       return n1+n2;
+    }
+    
+    static double multi(double n1, double n1)
+    {
+       return n1*n2;
+    }
+}
